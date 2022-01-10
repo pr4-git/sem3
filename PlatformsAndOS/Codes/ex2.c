2403eb76b0eefe4d4d08f30b652214ae9140a368
pr4-git <210019@softwarica.edu.np>
2022-01-10 13:20:43 +0545

    jan10-commit

--- PlatformsAndOS/Codes/ex2.c
+++ PlatformsAndOS/Codes/ex2.c
@@ -1,12 +1,9 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
     printf("We are in ex2 program\n");
     printf("PID of ex2 is %d\n", getpid());
-
 }
-
-
